bb385f4a5aa59881b73f4b36da371860f4596ca4
Mahedi-Hasan10 <hasanmdmahedi193@gmail.com>
2024-08-07 09:15:13 +0600

    backend codebase

---
+++ src/routes/user.routes.js
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
+router.route("/login", loginUser);
+
+// secure route
+router.route("/logout", verifyJWT, logoutUser);
+
+export default router;
