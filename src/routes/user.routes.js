fec8c41795e8b16223d1894cbc31d74ea7a987fe
Mahedi-Hasan10 <hasanmdmahedi193@gmail.com>
2024-08-12 15:12:03 +0600

    router added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   logoutUser,
   registerUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCover,
+  getUserChannelProfile,
+  getUserWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -25,7 +32,15 @@ router.route("/register").post(
 );
 router.route("/login").post(loginUser);
 // secure route
-router.route("/logout", verifyJWT, logoutUser);
-router.route("/refresh-token", refreshAccessToken);
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/profile").get(verifyJWT, getCurrentUser)
+router.route("/update-profile").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").post(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").post(verifyJWT,upload.single("coverImage"),updateUserCover)
+//get user chanel id from params
+router.route("/channel/:id").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getUserWatchHistory)
 
 export default router;
